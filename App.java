d8b83a771ac09bc4c5edc22c224ac7a96847d1bd
chris04003 <csecriticsnec@gmail.com>
2024-07-09 10:36:55 +0530

    hibernate.java

--- App.java
+++ App.java
@@ -1,13 +1,31 @@
 package hibernatedemo.hb;
+import com.example.model.User;
+import com.example.util.HibernateUtil;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class CreateUser {
+    public static void main(String[] args) {
+        Session session = HibernateUtil.getSessionFactory().openSession();
+        Transaction transaction = null;
+
+        try {
+            transaction = session.beginTransaction();
+            User user = new User();
+            user.setName("John Doe");
+            user.setEmail("john.doe@example.com");
+
+            session.save(user);
+            transaction.commit();
+        } catch (Exception e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+            e.printStackTrace();
+        } finally {
+            session.close();
+        }
+
+        HibernateUtil.shutdown();
     }
 }
